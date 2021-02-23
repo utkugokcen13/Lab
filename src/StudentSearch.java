f5a456e66b71401eab66cfed0d02db9911f42d60
Utku Gökçen <utku.gokcen@hotmail.com>
2021-02-23 16:05:03 +0300

    implement findAll method

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -20,7 +20,7 @@ public class StudentSearch {
 
         throw new Exception("There is no student with the given name!");
     }
-    public ArrayList<Student> findAll(ArrayList<Student> students, String name) throws Exception {
+    public ArrayList<Student> findAll(ArrayList<Student> students,String name) throws Exception {
         ArrayList<Student> result = new ArrayList<Student>();
         for (Student student: students)
             if (student.getName().equals(name)) {
